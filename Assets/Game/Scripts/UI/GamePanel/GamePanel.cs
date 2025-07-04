8aae5e3584b521ff79606279787daa464cb2b4a9
Tiêu Kiếm <107684026+realtieukiem@users.noreply.github.com>
2025-07-04 15:15:33 +0700

    update fix UI

--- Assets/Game/Scripts/UI/GamePanel/GamePanel.cs
+++ Assets/Game/Scripts/UI/GamePanel/GamePanel.cs
@@ -5,21 +5,25 @@ using UnityEngine;
 using UnityEngine.UI;
 using TMPro;
 
-public class GamePanel : UIFrame {
+public class GamePanel : UIFrame
+{
     [Header("[References]")]
     [SerializeField] private TextMeshProUGUI txtLevel;
     [SerializeField] private Button btnPause;
     [SerializeField] private TextMeshProUGUI txtCountDownTime;
-    private void Start() {
+    private void Start()
+    {
         btnPause.onClick.AddListener(PauseGame);
         EventDispatcher.AddListener<GameEvent.LevelTimeChanged>(UpdateCountdownTime);
         EventDispatcher.AddListener<GameEvent.LevelCountdownChanged>(LevelCountdownChanged);
     }
-    private void OnDestroy() {
+    private void OnDestroy()
+    {
         EventDispatcher.RemoveListener<GameEvent.LevelTimeChanged>(UpdateCountdownTime);
         EventDispatcher.RemoveListener<GameEvent.LevelCountdownChanged>(LevelCountdownChanged);
     }
-    private void LevelCountdownChanged(GameEvent.LevelCountdownChanged args) {
+    private void LevelCountdownChanged(GameEvent.LevelCountdownChanged args)
+    {
         /*if (args.IsPaused) {
             spinTime.gameObject.SetActive(true);
             spinTime.SetAnim(0, "frezee", null);
@@ -35,37 +39,50 @@ public class GamePanel : UIFrame {
         */
 
     }
-    protected override void OnShow(bool instant = false) {
+    protected override void OnShow(bool instant = false)
+    {
         base.OnShow(instant);
 
         txtLevel.text = string.Format("LEVEL{0}", GameController.Instance.LoadLevelOption.Level);
     }
 
-    protected override void OnBack() {
+    protected override void OnBack()
+    {
         PauseGame();
     }
 
-    private void PauseGame() {
+    private void PauseGame()
+    {
         UIManager.Instance.Push<PausePanel>();
     }
-    public void SetCountdownTime(int seconds) {
+    public void SetCountdownTime(int seconds)
+    {
         TimeSpan timeSpan = TimeSpan.FromSeconds(seconds);
-        if (timeSpan.TotalHours > 1) {
+        if (timeSpan.TotalHours > 1)
+        {
             txtCountDownTime.text = timeSpan.ToString(@"hh\:mm\:ss");
-        } else {
+        }
+        else
+        {
             txtCountDownTime.text = timeSpan.ToString(@"mm\:ss");
         }
     }
-    public void UpdateCountdownTime(GameEvent.LevelTimeChanged args) {
+    public void UpdateCountdownTime(GameEvent.LevelTimeChanged args)
+    {
         SetCountdownTime(args.RemainingSeconds);
-        if (args.RemainingSeconds == 0) {
+        if (args.RemainingSeconds == 0)
+        {
             //ConfigDatabase.Instance.AudioConfig.lastTickSound.Play();
             //warringVfx.SetActive(false);
-        } else if (args.RemainingSeconds <= 10) {
-            txtCountDownTime.color = Color.red;
+        }
+        else if (args.RemainingSeconds <= 10)
+        {
+            //txtCountDownTime.color = Color.red;
             //ConfigDatabase.Instance.AudioConfig.tickSound.Play();
             //warringVfx.SetActive(true);
-        } else {
+        }
+        else
+        {
             txtCountDownTime.color = Color.white;
             //warringVfx.SetActive(false);
         }
